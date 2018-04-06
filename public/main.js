3a1b267765f0cb702fe6c837e4b2d477a4fafc33
unknown <Dan Edwards@DEDWARDS-T470p.netroadshow.com>
2018-04-06 15:31:43 -0400

    Add verto to conf app

--- public/main.js
+++ public/main.js
@@ -1,65 +1,21 @@
-$(function() {
-  var FADE_TIME = 150; // ms
-  var TYPING_TIMER_LENGTH = 400; // ms
-  var COLORS = [
-    '#e21400', '#91580f', '#f8a700', '#f78b00',
-    '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
-    '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
-  ];
 
-  // Initialize variables
-  var $window = $(window);
-  
-  var $usernameInput = $('.usernameInput'); // Input for username
+function joinConference() {
+  console.log('join conference called');
+  $.verto.makeCall('3010');
+}
 
-  var $loginPage = $('.login.page'); // The login page
-  var $chatPage = $('.chat.page'); // The chatroom page
-
-  var $currentInput = $usernameInput.focus();
-
-  var socket = io();
-
-
-  // Log a message
-  function log (message, options) {
-    var $el = $('<li>').addClass('log').text(message);
-    addMessageElement($el, options);
+var wrtc_app = new Vue({
+  el: '#wrtc_app',
+  data: {
+    conferences: []
+  },
+  methods: {
+    joinConference: joinConference
   }
+})
 
-  // Adds a message element to the messages and scrolls to the bottom
-  // el - The element to add as a message
-  // options.fade - If the element should fade-in (default = true)
-  // options.prepend - If the element should prepend
-  //   all other messages (default = false)
-  function addMessageElement (el, options) {
-    var $el = $(el);
-
-    // Setup default options
-    if (!options) {
-      options = {};
-    }
-    if (typeof options.fade === 'undefined') {
-      options.fade = true;
-    }
-    if (typeof options.prepend === 'undefined') {
-      options.prepend = false;
-    }
-
-    // Apply options
-    if (options.fade) {
-      $el.hide().fadeIn(FADE_TIME);
-    }
-    if (options.prepend) {
-      $messages.prepend($el);
-    } else {
-      $messages.append($el);
-    }
-    $messages[0].scrollTop = $messages[0].scrollHeight;
-  }
-
-  // Prevents input from having injected markup
-  function cleanInput (input) {
-    return $('<div/>').text(input).html();
-  }
-
+var sio = io();
+sio.on('conference list', (c_list) => {
+  wrtc_app.conferences = c_list;
 });
+
