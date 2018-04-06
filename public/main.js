2acb78bd6a2fb0fe9c2c752a6a80df465a639c06
unknown <Dan Edwards@DEDWARDS-T470p.netroadshow.com>
2018-04-06 15:45:14 -0400

    Trying to get to verto object

--- public/main.js
+++ public/main.js
@@ -1,7 +1,8 @@
 
 function joinConference() {
   console.log('join conference called');
-  $.verto.makeCall('3010');
+  debugger;
+  verto.newCall('3010');
 }
 
 var wrtc_app = new Vue({
@@ -18,4 +19,30 @@ var sio = io();
 sio.on('conference list', (c_list) => {
   wrtc_app.conferences = c_list;
 });
+
+var callbacks = {
+  onMessage: ( verto, dialog, msg, data) => {
+    console.log(`onMessage: ${msg}`);
+  },
+
+  onDialogState: d => {
+    console.log(`onDialogState: d=${JSON.stringify(d)}`)
+  },
+
+  onWSLogin: (v, success) => {
+    console.log(`onWSLogin: v=${JSON.stringify(v)}`);
+  },
+
+  onWSClose: (v, success) => {
+    console.log(`onWSClose: v=${JSON.stringify(v)}`);
+  }
+}
+
+var verto = new $.verto({
+  login: '1000@verto.nrs.com',
+  passwd: '1122',
+  socketUrl: 'wss://ec2-18-222-61-84.us-east-2.compute.amazonaws.com:8082',
+  tag: "audio",
+  iceServers: true
+}, callbacks);
 
