5ae737ddb160f349f365977974585e7aae1902ab
DEDWARDS-T470P\Dan Edwards <dedwards@netroadshow.com>
2018-05-03 18:30:17 -0400

    Building out demo app

--- source/index.ts
+++ source/index.ts
@@ -1,7 +1,8 @@
 // Setup basic express server
 import express = require('express');
 import path = require('path');
-import { FreeSWITCH } from './freeswitch';
+import util = require('util');
+//import { FreeSWITCH } from './freeswitch';
 
 var app = express();
 var server = require('http').createServer(app);
@@ -21,37 +22,4 @@ server.listen(port, function () {
 // Routing
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.static(path.join(__dirname, '..', 'node_modules')));
-
-var numUsers = 0;
-var esl_conn = new FreeSWITCH.FSConnection( ESL_HOST, ESL_PORT, ESL_PWD );
-
-esl_conn.on('connected', () => {
-  console.log('FS connection established.');
-
-  if (!io) {
-    io = require('socket.io')(server);
-
-    io.on('connection', function (socket) {
-      console.log(`Processing socket connection from ${socket.id}`);
-
-      esl_conn.conference_list()
-        .then( c_list => {
-          socket.emit('conference list', c_list);
-        })
-
-      socket.on('join conference', function (dn) {
-        console.log(`User wants to join conference ${dn}`);
-      });
-
-      socket.on('leave conference', function (dn) {
-          console.log(`User leaving conference ${dn}`)
-      });
-
-      socket.on('disconnect', function () {
-          console.log(`disconnect: ${socket.id}`);
-      });
-    });
-  }
-});
-
 
